eedc6c617e78e4f8c4a5e27098e9891be3471b74
Dinesh Maharjan <httpdeveloper@gmail.com>
2017-07-13 18:32:29 +0545

    Adding test file

---
+++ test/setup.js
@@ -0,0 +1,11 @@
+jest.mock('Linking', () => {
+  return {
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    openURL: jest.fn(),
+    canOpenURL: jest.fn(),
+    getInitialURL: jest.fn(),
+  };
+});
+
+jest.mock('react-native-camera', () => 'Camera');
